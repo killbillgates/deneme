4492a43ae7b24434eb9821540a56e1023c7138ef
killbillgates <noreply>
2015-07-14 14:43:27 +0000

    Add component asdasdas

---
+++ ui/asdasdas.reel/asdasdas.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/asdasdas.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Asdasdas
+ * @extends Component
+ */
+exports.Asdasdas = Component.specialize(/** @lends Asdasdas# */ {
+    constructor: {
+        value: function Asdasdas() {
+            this.super();
+        }
+    }
+});
